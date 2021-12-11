d1abb7eab03362883e8c9762c61eb4d63ccbee0c
Evgeniy Karasev <karasevevg@gmail.com>
2021-12-11 12:19:21 +0300

    update model

--- src/main/java/com/cronnoss/api/domain/Billing.java
+++ src/main/java/com/cronnoss/api/domain/Billing.java
@@ -1,14 +1,28 @@
 package com.cronnoss.api.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.util.Set;
+public class Billing implements Serializable {
+    private Card card;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 6577338081290507077L;
 
-@Getter
-@Setter
-public class Billing {
+    public Card getCard() {
+        return card;
+    }
 
-    private String id;
-    private Set<Card> card;
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
--- src/main/java/com/cronnoss/api/domain/Name.java
+++ src/main/java/com/cronnoss/api/domain/Name.java
@@ -1,14 +1,47 @@
 package com.cronnoss.api.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
-@Getter
-@Setter
-public class Name {
+public class Name implements Serializable {
 
-    private String id;
     private String title;
     private String first;
     private String last;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 420620315591775395L;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
 }
