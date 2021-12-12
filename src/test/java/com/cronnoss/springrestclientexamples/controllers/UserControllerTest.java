d6d5547c0cb77542d312a67b2caf40c512960b31
Evgeniy Karasev <karasevevg@gmail.com>
2021-12-12 12:51:34 +0300

    8. Using WebFlux to Display API Data

---
+++ src/test/java/com/cronnoss/springrestclientexamples/controllers/UserControllerTest.java
@@ -0,0 +1,52 @@
+package com.cronnoss.springrestclientexamples.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UserControllerTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    WebTestClient webTestClient;
+
+    @BeforeEach
+    public void setUp() {
+        webTestClient = WebTestClient.bindToApplicationContext(applicationContext).build();
+    }
+
+    @Test
+    public void index() {
+
+        webTestClient.get().uri("/")
+                .exchange()
+                .expectStatus().is2xxSuccessful();
+    }
+
+    @Disabled
+    @Test
+    public void formPost() {
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("limit", "1");
+
+        webTestClient.post().uri("users")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(formData))
+                .exchange()
+                .expectStatus().is2xxSuccessful();
+    }
+}
